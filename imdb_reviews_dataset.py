209592c9ca4dbe755b5799e1456eab6dd6d4058b
Usama Mohiuddin <usamamohiudin@gmail.com>
2023-11-11 22:55:14 -0500

    added code that aligns more with project instructions and also added a subset function to imdb reviews for testing data faster in less data using a subset instead of the full dataset

--- imdb_reviews_dataset.py
+++ imdb_reviews_dataset.py
@@ -1,56 +1,66 @@
 import os
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.model_selection import train_test_split
-
-from sklearn.datasets import make_classification
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.svm import LinearSVC
 from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier
-
-
-#--------------------------------------------------------------------------------------------------------------------
-#JUST A PLACEHOLDER, CHANGE THE CODE BELOW IF NEEDED
-#--------------------------------------------------------------------------------------------------------------------
-
+from sklearn.model_selection import train_test_split
 
 
 # Function to read data
-def read_data(path):
+# If use_subset is True, then it only read a dataset, if its false it reads the full dataset
+# This use subset function was added so that testing was easier to do faster with this large dataset
+def read_data(path, use_subset=False, max_per_category=100):
+
     texts, labels = [], []
     for label in ['pos', 'neg']:
         dir_path = os.path.join(path, label)
-        for file in os.listdir(dir_path):
-            with open(os.path.join(dir_path, file), 'r', encoding='utf-8') as f:
-
+        files = os.listdir(dir_path)
+        # If use_subset is True, limit the number of files read
+        if use_subset:
+            files = files[:max_per_category]
+        for file in files:
+            file_path = os.path.join(dir_path, file)
+            with open(file_path, 'r', encoding='utf-8') as f:
                 texts.append(f.read())
                 labels.append(1 if label == 'pos' else 0)
     return texts, labels
+
+# Specify the path to your dataset
+train_path = 'aclImdb/train'
+test_path = 'aclImdb/test'
+
+# Use the modified read_data function to read a subset of the dataset
+#To read the full dataset also change use_subset to False so that it read the full dataset
+train_texts, train_labels = read_data(train_path, use_subset=False, max_per_category=10)
+test_texts, test_labels = read_data(test_path, use_subset=False, max_per_category=10)
+
 
 # Reading Data
 train_texts, train_labels = read_data('aclImdb/train')
 test_texts, test_labels = read_data('aclImdb/test')
 
-# Vectorize texts
-vectorizer = CountVectorizer()
+# Vectorize texts with TF-IDF
+vectorizer = TfidfVectorizer(stop_words='english')
 X_train = vectorizer.fit_transform(train_texts)
 X_test = vectorizer.transform(test_texts)
 
-#Create Random models
+# Create and train models
 models = {
-    "Logistic Regression": LogisticRegression(),
+    "Logistic Regression": LogisticRegression(max_iter=1000),
     "Decision Tree": DecisionTreeClassifier(),
-    "SVM": LinearSVC(),
+    "SVM": LinearSVC(dual=False,  max_iter=1000),
     "AdaBoost": AdaBoostClassifier(),
     "Random Forest": RandomForestClassifier()
 }
 
-#Train, predict and evaluate random models
+# Train, predict and evaluate models
 for name, model in models.items():
     model.fit(X_train, train_labels)
     y_pred = model.predict(X_test)
-    acc = accuracy_score(test_labels, y_pred)
-    print(f"{name} Accuracy: {acc}")
-
+    accuracy = accuracy_score(test_labels, y_pred)
+    precision = precision_score(test_labels, y_pred)
+    recall = recall_score(test_labels, y_pred)
+    f1 = f1_score(test_labels, y_pred)
+    print(f"{name} - Accuracy: {accuracy}, Precision: {precision}, Recall: {recall}, F1 Score: {f1}")
--- newsgroup_dataset.py
+++ newsgroup_dataset.py
@@ -1,87 +1,79 @@
 from sklearn.datasets import fetch_20newsgroups
-from sklearn.datasets import make_classification
-from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import accuracy_score
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.svm import LinearSVC
 from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier
+from sklearn.model_selection import train_test_split
 
-#--------------------------------------------------------------------------------------------------------------------
-#JUST A PLACEHOLDER, CHANGE THE CODE BELOW IF NEEDED
-#--------------------------------------------------------------------------------------------------------------------
+# Load the 20 newsgroups dataset
+categories = ['alt.atheism', 'comp.graphics', 'sci.med', 'soc.religion.christian']
+newsgroups_train = fetch_20newsgroups(subset='train', categories=categories, remove=('headers', 'footers', 'quotes'))
+newsgroups_test = fetch_20newsgroups(subset='test', categories=categories, remove=('headers', 'footers', 'quotes'))
 
-#Generate Random Data
-X, y = make_classification(n_samples=1000, n_features=20)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+# TF-IDF Vectorization
+vectorizer = TfidfVectorizer(stop_words='english')
+X_train = vectorizer.fit_transform(newsgroups_train.data)
+X_test = vectorizer.transform(newsgroups_test.data)
+y_train = newsgroups_train.target
+y_test = newsgroups_test.target
 
-#Create Random models
+# Define models
 models = {
     "Logistic Regression": LogisticRegression(),
     "Decision Tree": DecisionTreeClassifier(),
-    "SVM": LinearSVC(),
+    "SVM": LinearSVC(dual=False),
     "AdaBoost": AdaBoostClassifier(),
     "Random Forest": RandomForestClassifier()
 }
 
-#Train, predict and evaluate random models
+# Train, predict, and evaluate models
 for name, model in models.items():
-    # Train
     model.fit(X_train, y_train)
-    # Predict
     y_pred = model.predict(X_test)
-    # Evaluate
-    acc = accuracy_score(y_test, y_pred)
-    print(f"{name} Accuracy: {acc}")
-
-
+    accuracy = accuracy_score(y_test, y_pred)
+    print(f"{name} Accuracy: {accuracy}")
 
 
 
 
 #--------------------------------------------------------------------------------------------------------------------
-#JUST AN EXAMPLE FOR NEWSGROUP
-#--------------------------------------------------------------------------------------------------------------------
+#THE CODE BELOW WAS GIVEN BY THE DATASET ITSELF TO USE FOR IMPLEMENTATION IF NEEDED
+# #--------------------------------------------------------------------------------------------------------------------
 
-#this code was given by the newsgroup tutorial website posted in the assignment instructions
-#https://scikit-learn.org/stable/tutorial/text_analytics/working_with_text_data.html
-
-
-categories = ['alt.atheism', 'soc.religion.christian',
-              'comp.graphics', 'sci.med']
-
-#just testing to see if the function works,  takes too long to fetch data
-print('start fetching')
-twenty_train = fetch_20newsgroups(subset='train', categories=categories, shuffle=True, random_state=42)
-print('stop fetching')
+# #this code was given by the newsgroup tutorial website posted in the assignment instructions
+# #https://scikit-learn.org/stable/tutorial/text_analytics/working_with_text_data.html
 
 
-#prints Target names
-print(twenty_train.target_names)               
+# categories = ['alt.atheism', 'soc.religion.christian',
+#               'comp.graphics', 'sci.med']
 
-#prints the length of data and filename
-print(len(twenty_train.data))                  
-print(len(twenty_train.filenames))
-
-
-#prints the first 3 lines of the first data file
-print("\n".join(twenty_train.data[0].split("\n")[:3]))
-
-#print target name of the first data file
-print(twenty_train.target_names[twenty_train.target[0]])
-
-#print first 10 targets
-print(twenty_train.target[:10])
-
-#print target name of the first 10 data files
-for t in twenty_train.target[:10]:
-        print(twenty_train.target_names[t])
+# #just testing to see if the function works,  takes too long to fetch data
+# print('start fetching')
+# twenty_train = fetch_20newsgroups(subset='train', categories=categories, shuffle=True, random_state=42)
+# print('stop fetching')
 
 
+# #prints Target names
+# print(twenty_train.target_names)               
 
-#--------------------------------------------------------------------------------------------------------------------
-#BEGIN coding below, above are just examples 
-#--------------------------------------------------------------------------------------------------------------------
+# #prints the length of data and filename
+# print(len(twenty_train.data))                  
+# print(len(twenty_train.filenames))
 
+
+# #prints the first 3 lines of the first data file
+# print("\n".join(twenty_train.data[0].split("\n")[:3]))
+
+# #print target name of the first data file
+# print(twenty_train.target_names[twenty_train.target[0]])
+
+# #print first 10 targets
+# print(twenty_train.target[:10])
+
+# #print target name of the first 10 data files
+# for t in twenty_train.target[:10]:
+#         print(twenty_train.target_names[t])
 
 
